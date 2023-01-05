b87a92d8c2369ceef6ee30b2339f2a5063719bca
JassminDelic <jassmindelic@gmail.com>
2023-01-05 13:39:06 +0100

    Added Campground CRUD

---
+++ seeds/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+async function main() {
+	await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+	console.log("Database connected.");
+}
+
+main().catch((err) => console.log(err));
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+	await Campground.deleteMany({});
+	for (let i = 0; i < 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		const camp = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${sample(descriptors)} ${sample(places)}`,
+		});
+		await camp.save();
+	}
+};
+
+seedDB().then(() => mongoose.connection.close());
